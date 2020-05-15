b60bc89c65d4d50930bad68543b4bab38c467468
astro30 <22853551+astro30@users.noreply.github.com>
2020-05-15 10:37:41 +0200

    Add babel  with react

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,40 @@
 import typescript from '@rollup/plugin-typescript';
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 
 const dist = 'dist';
+const bundle = 'bundle';
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: 'src/index.ts',
+  input: 'src/index.tsx',
   output: [
     {
-      file: `${dist}/bundle.cjs.js`,
+      file: `${dist}/${bundle}.cjs.js`,
       format: 'cjs',
     },
     {
-      file: `${dist}/bundle.esm.js`,
+      file: `${dist}/${bundle}.esm.js`,
       format: 'esm',
     },
     {
       name: 'ReactCssSpinners',
-      file: `${dist}/bundle.umd.js`,
+      file: `${dist}/${bundle}.umd.js`,
       format: 'umd',
+      globals: {
+        react: 'React',
+      },
     },
   ],
-  plugins: [typescript()],
+  plugins: [
+    resolve(),
+    typescript(),
+    babel({
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled',
+    }),
+    production && terser(),
+  ],
+  external: ['react'],
 };
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React, { ReactElement, FunctionComponent } from 'react';
+
+const Spinner: FunctionComponent = (): ReactElement => <h1>Spinner</h1>;
+
+export default Spinner;
